57035109e286a4042c5cae12f6e3dca49f9ee324
ju yeonjin <yeonchan0321@naver.com>
2022-07-05 22:56:40 +0900

    7월 5일 최종 커밋

--- DREAMCODING/main.js
+++ DREAMCODING/main.js
@@ -31,6 +31,51 @@ homeContactBtn.addEventListener("click", () => {
 
 });
 
+//make home slowly fade to transparnet as the window scrolls down
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.screenY / homeHeight;
+});
+
+
+// show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+if(window.screenY > homeHeight/2){
+  arrowUp.classList.add('visible');
+} else {
+  arrowUp.classList.remove('visible');
+}
+});
+
+//handle click on the "arrow up" button
+
+arrowUp.addEventListener('click', () => {
+scrollIntoView('#home');
+});
+
+//프로젝트
+
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if(filter == null){
+    return;
+  }
+  projects.forEach((project) => {
+    console.log(project.dataset.type);
+    if(filter === '*' || filter === project.dataset.type) {
+      project.classList.remove('invisible');
+    } else {
+      project.classList.add('invisible');
+    }
+  });
+});
+
 /* 스크롤 함수 만들기 */
 function scrollIntoView(selector){
   const scrollTo = document.querySelector(selector);
