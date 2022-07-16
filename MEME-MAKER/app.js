bd3332e6836f43e4f0a0c5c954e9b11476751274
ju yeonjin <yeonchan0321@naver.com>
2022-07-16 22:53:28 +0900

    바닐라js 앱만들기

--- MEME-MAKER/app.js
+++ MEME-MAKER/app.js
@@ -5,3 +5,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800; // 이미지 퀄리티를 높이기 위해서 설정해줌
 
+ctx.fillRect(210-40,200-20,15,100)
+ctx.fillRect(350-40,200-20,15,100)
+ctx.fillRect(260-40,200-20,60,200)
+
+ctx.arc(250, 100, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillSty= "white";
+ctx.arc(260 + 10 , 80, 8, Math.PI, );
+ctx.arc(220 + 10 , 80, 8, Math.PI, );
+ctx.fill();
